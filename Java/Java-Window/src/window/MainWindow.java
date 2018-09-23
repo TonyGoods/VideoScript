5663ef61bf9ee9483a8f24b74aff5c36593310f4
Tony Goods <ZGuo32@its.jnj.com>
2018-09-23 22:24:09 +0800

    java 下载.ts文件还是有点问题，试试python方法。

--- Java/Java-Window/src/window/MainWindow.java
+++ Java/Java-Window/src/window/MainWindow.java
@@ -8,6 +8,20 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 import java.awt.FlowLayout;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -26,11 +40,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MainWindow extends JFrame {
 
@@ -140,23 +158,84 @@ public class MainWindow extends JFrame {
 					JOptionPane.showMessageDialog(null, "请将信息填写完整！");
 					return;
 				} else {
-					try {
-						URL url = new URL("https://lens.zhihu.com/api/videos/994904999301885952");
-						HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
-						if (https.getResponseCode() == 200) {
-							System.out.println("Yes!");
-							BufferedReader reader = new BufferedReader(new InputStreamReader(https.getInputStream()));
-							String line;
-							while ((line = reader.readLine()) != null) {
-								System.out.println(line);
+					WebDriver webDriver = new HtmlUnitDriver();
+					webDriver.get("https://www.zhihu.com/question/56101818/answer/485899657");
+					String title = webDriver.getTitle();
+					System.out.println(webDriver.getPageSource());
+					String html = webDriver.getPageSource();
+					Document soup = Jsoup.parse(html);
+					Elements as = soup.getElementsByTag("a");
+					String webLinkTitle = "https://lens.zhihu.com/api/videos/";
+					for (Element a : as) {
+						if (a.hasAttr("data-lens-id")) {
+							System.out.println(a.attr("data-lens-id"));
+							try {
+								URL url = new URL(webLinkTitle + a.attr("data-lens-id"));
+								HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+								if (httpsURLConnection.getResponseCode() == 200) {
+									BufferedReader reader = new BufferedReader(
+											new InputStreamReader(httpsURLConnection.getInputStream()));
+									String line = reader.readLine();
+									JSONParser parser = new JSONParser();
+									JSONObject json = (JSONObject) parser.parse(line);
+									JSONObject playlist = (JSONObject) json.get("playlist");
+									JSONObject ld = (JSONObject) playlist.get("ld");
+									String m3u8Link = (String) ld.get("play_url");
+									String tsWebLinkTitle = m3u8Link.substring(0, m3u8Link.lastIndexOf('/') + 1);
+									System.out.println(tsWebLinkTitle);
+									System.out.println(m3u8Link);
+									URL m3u8URL = new URL(m3u8Link);
+									HttpsURLConnection m3u8Https = (HttpsURLConnection) m3u8URL.openConnection();
+									int index = 1;
+									if (m3u8Https.getResponseCode() == 200) {
+										BufferedReader m3u8Reader = new BufferedReader(
+												new InputStreamReader(m3u8Https.getInputStream()));
+										String m3u8Line;
+
+										while ((m3u8Line = m3u8Reader.readLine()) != null) {
+											if (m3u8Line.indexOf('#') != 0) {
+												webDriver.get(tsWebLinkTitle + m3u8Line);
+												
+												File file = new File(
+														"C:\\Users\\Administrator\\Desktop\\" + title + index + ".ts");
+												if (file.exists()) {
+													file.delete();
+												}
+												file.createNewFile();
+												URL tsURL = new URL(tsWebLinkTitle + m3u8Line);
+												System.out.println(tsWebLinkTitle + m3u8Line);
+												HttpsURLConnection tsUrlConnection = (HttpsURLConnection) tsURL
+														.openConnection();
+												if (tsUrlConnection.getResponseCode() == 200) {
+													BufferedReader tsReader = new BufferedReader(
+															new InputStreamReader(tsUrlConnection.getInputStream()));
+													BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+													String tsLine;
+													while ((tsLine = tsReader.readLine()) != null) {
+														writer.write(webDriver.getPageSource());
+														// writer.write(tsLine);
+														// System.out.println(tsLine);
+													}
+													writer.flush();
+													writer.close();
+													tsReader.close();
+												}
+												index++;
+											}
+										}
+									}
+								}
+							} catch (MalformedURLException e1) {
+								e1.printStackTrace();
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							} catch (ParseException e1) {
+								e1.printStackTrace();
 							}
 						}
-					} catch (MalformedURLException e1) {
-						e1.printStackTrace();
-					} catch (IOException e1) {
-						e1.printStackTrace();
 					}
 				}
+
 			}
 		});
 		pnlSave.add(btnStart);
